1d4c024d8c1666b10cc88ecc74e5c8b9801a1328
zsxlesz <136483423+zsxlesz@users.noreply.github.com>
2023-11-17 16:18:24 +0100

    initial commit
    alap struktúra +  index.php

---
+++ web2-bead2-Db.sql
@@ -0,0 +1,11 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Adatbázis: `cukraszda`
+--
+CREATE DATABASE IF NOT EXISTS `cukraszda` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `cukraszda`;
+
+-- --------------------------------------------------------
