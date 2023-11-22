6e02d7ae7b231ab47ceffae48c6617a3096f7bdd
Vicziand <113669347+Vicziand@users.noreply.github.com>
2023-11-22 18:38:06 +0100

    Create idopont_foglalas_main.php

---
+++ views/idopont_foglalas_main.php
@@ -0,0 +1,16 @@
+<section class="py-5">
+    <div class="container px-4 px-lg-5 mt-5">
+<div id="tabla-container"></div>
+        <div id="demo"></div>
+    </div>
+
+</section>
+<script type="text/javascript">
+    <?php require_once("Javascript/dto/szabad_idopontok_dto.js");?>
+</script>
+<script type="text/javascript">
+    <?php require_once("Javascript/dto/table_dto.js");?>
+</script>
+<script type="text/javascript">
+    <?php require_once("Javascript/to_view/table.js");?>
+</script>
